889ed62dd6cb5e3fb6014ea27897e9cd2fa4bdf6
Snare <sh0n556@bk.ru>
2022-12-10 15:49:59 +0300

    nothing works yet

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
+#include "Type.h"
+
+#include <memory>
+
 
 int main()
 {
+	Int var = 1;
 
+	Int var2 = 2;
+
+	var2 = var;
+	var = 123;
+
+	std::cout << *var << "\n";
+	std::cout << *var2 << "\n";
 
 	return 0;
 }
