7bfd510aa4fd780009bdc6c96ab67eef2f327b2d
Letinemeso <sh0n556@bk.ru>
2024-02-19 18:58:57 +0300

    added macros for builder stub initialization

--- include/Builder_Stub.h
+++ include/Builder_Stub.h
@@ -5,6 +5,33 @@
 #include <Variable_Base.h>
 
 
+#ifndef BUILDER_STUB
+#define BUILDER_STUB
+
+    #define INIT_BUILDER_STUB(Type_To_Construct) \
+    protected:  \
+        using Constructed_Type = Type_To_Construct;     \
+        LV::Variable_Base* M_construct_product() const override;    \
+        void M_init_constructed_product(LV::Variable_Base* _product) const override;    \
+    public:
+
+
+    #define BUILDER_STUB_CONSTRUCTION_FUNC(Stub_Type)  LV::Variable_Base* Stub_Type::M_construct_product() const
+
+    #define BUILDER_STUB_CONSTRUCTION_FUNC_DEFAULT_IMPL { return new Constructed_Type; }
+
+    #define BUILDER_STUB_CONSTRUCTION_FUNC_CUSTOM_IMPL(RETURN_VALUE) { return RETURN_VALUE; }
+
+
+    #define BUILDER_STUB_INITIALIZATION_FUNC(Stub_Type) void Stub_Type::M_init_constructed_product(LV::Variable_Base* _product) const
+
+    #define BUILDER_STUB_PARENT_INITIALIZATION Parent_Type::M_init_constructed_product(_product)
+
+    #define BUILDER_STUB_CAST_PRODUCT Constructed_Type* product = (Constructed_Type*)_product
+
+#endif
+
+
 namespace LV
 {
 
